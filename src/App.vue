9fc0928a4d3f0d871fb784caa27b35446a51f4b3
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-12-13 00:47:56 +0300

    add learning resources component

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+      <learning-resource v-for="res in storedResources" 
+      :key="res.id" 
+      :title="res.title" 
+      :description="res.description"
+      :link="res.link"
+      ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource'
+
+
+export default {
+    components: {
+        LearningResource
+    },
+    data() {
+        return {
+            storedResources: [
+                { 
+                    id: 'official-guide', 
+                    title: 'official guide', 
+                    description: 'the official vue js documentation',
+                    link: 'https://vuejs.org'
+                },
+                { 
+                    id: 'google', 
+                    title: 'Google', 
+                    description: 'Learn to google...',
+                    link: 'www.google.com'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
